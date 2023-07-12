9a46278f74f2ccdc3eff6e49f7c01ee3dd36087e
Dhiraj kumar <kumardhiraj0248@gmail.com>
2023-07-12 22:48:38 +0530

    done qn 1

---
+++ question.js
@@ -0,0 +1,7 @@
+
+    function changeColorAndSize() {
+      var box = document.querySelector('.box');
+      box.style.backgroundColor = 'yellow';
+      box.style.width = '600px';
+      box.style.height = '600px';
+    }
